5a911f84ea9707418231d4d8873bd56c89b3a97e
Someone <rgbjkbs@hdbf.ru>
2016-12-29 20:15:19 +0200

    create index.php, init session

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        if(empty($_SESSION['login'])):?>
+        <?php else:?>
+        <?php endif;?>
+        ?>
+    </body>
+</html>
