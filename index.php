de9c70b008148b85b5bab5d83233495dba334bd6
Someone <rgbjkbs@hdbf.ru>
2016-12-29 20:41:07 +0200

    log in and log out

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 session_start();
+
+//log in form hander
+$login=$_POST['login'];
+if(!empty($login)){
+    $_SESSION['login']=$login;
+}
+//============
+
+//exit
+if(isset($_GET['action'])){
+    session_destroy();
+    header('Location:'.$_SERVER['PHP_SELF']);
+}
+//=============
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,8 +22,7 @@ session_start();
         <title></title>
     </head>
     <body>
-        <?php
-        if(empty($_SESSION['login'])):?>
+        <?php if(empty($_SESSION['login'])):?>
         <form method="post">
             <label>Login
                 <input type="text" name="login"/>
@@ -17,7 +30,8 @@ session_start();
             <input type="submit" name="log in"/>
         </form>
         <?php else:?>
+        <?= $_SESSION['login']?>
+        <a href='?action=exit'>Log out</a>
         <?php endif;?>
-        ?>
     </body>
 </html>
